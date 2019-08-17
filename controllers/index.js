1bd40b9b4277c6fc4ce889d03958820d9d73864c
JohnLi1989 <lj85046673@hotmail.com>
2019-08-18 10:37:59 +1200

    a

---
+++ controllers/index.js
@@ -0,0 +1,12 @@
+const axios = require('axios');
+
+// Make a request for a user with a given ID
+axios.get('https://api.openaq.org/v1/countries')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  });
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,39 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  axios.get('https://api.openaq.org/v1/countries?limit=1000')
+  .then(function (response) {
+    // handle success
+    res.render('index', {cities:response.data.results});
+    //console.log(response.data);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  });
+  
+});
+
+
+router.get('/:city', function(req, res, next) {
+  var city = req.params.city;
+  console.log(city);
+  axios.get('https://api.openaq.org/v1/latest?city='+city)
+  .then(function (response) {
+    // handle success
+    res.render('city', {results:response.data.results, city: city});
+    //console.log(response.data);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  });
+  
 });
 
 module.exports = router;
