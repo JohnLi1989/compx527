4d20d3a5094d6a869f9ad3827f79313b2bf0df9f
JohnLi1989 <lj85046673@hotmail.com>
2019-08-25 12:54:17 +1200

    a

--- server/s3.js
+++ server/s3.js
@@ -26,23 +26,51 @@
 
 // snippet-start:[s3.JavaScript.buckets.listObjects]
 // Load the AWS SDK for Node.js
-var AWS = require('aws-sdk');
-// Set the region 
-AWS.config.update({region: 'us-east-1'});
+// var AWS = require('aws-sdk');
+// // Set the region 
+// AWS.config.update({region: 'us-east-1'});
 
-// Create S3 service object
-s3 = new AWS.S3();
+// // Create S3 service object
+// s3 = new AWS.S3();
 
-// Create the parameters for calling listObjects
-var bucketParams = {
-  Bucket : 'openaq-fetches',
+// // Create the parameters for calling listObjects
+// var bucketParams = {
+//   Bucket : 'openaq-fetches',
+// };
+
+// // Call S3 to obtain a list of the objects in the bucket
+// s3.listObjects(bucketParams, function(err, data) {
+//   if (err) {
+//     console.log("Error", err);
+//   } else {
+//     console.log("Success", data);
+//   }
+// });
+
+
+var AWS = require("aws-sdk");
+
+AWS.config.update({
+  region: "us-west-1",
+  endpoint: "http://localhost:3000"
+});
+
+var docClient = new AWS.DynamoDB.DocumentClient();
+
+var table = "airquality";
+
+var data = {"date":{"utc":"2019-08-20T02:00:00.000Z","local":"2019-08-20T10:00:00+08:00"},"parameter":"no2","value":35.8,"unit":"µg/m³","averagingPeriod":{"value":1,"unit":"hours"},"location":"Central/Western","city":"Central & Western","country":"HK","coordinates":{"longitude":114.14444444444445,"latitude":22.285},"attribution":[{"name":"Environmental Protection Department","url":"https://data.gov.hk/en-data/dataset/hk-epd-airteam-past24hr-pc-of-individual-air-quality-monitoring-stations"}],"sourceName":"Hong Kong","sourceType":"government","mobile":false}
+
+var params = {
+    TableName:table,
+    Item: data
 };
 
-// Call S3 to obtain a list of the objects in the bucket
-s3.listObjects(bucketParams, function(err, data) {
-  if (err) {
-    console.log("Error", err);
-  } else {
-    console.log("Success", data);
-  }
+console.log("Adding a new item...");
+docClient.put(params, function(err, data) {
+    if (err) {
+        console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        console.log("Added item:", JSON.stringify(data, null, 2));
+    }
 });
